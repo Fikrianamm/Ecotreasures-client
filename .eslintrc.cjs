79a8f9885a77cd169d8107d6a14f426457b5da99
fikrianamm <fikritaufiqulanam1@gmail.com>
2024-02-28 19:19:55 +0700

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb', 'airbnb/hooks', 'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 className="underline text-sky-500">Hello WOrld</h1>
+  );
+}
+
+export default App;
